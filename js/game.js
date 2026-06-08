f3edaabb101cd153486da2be496b78f1d3e434a5
SemyonMelentev <149328529+SemyonMelentev@users.noreply.github.com>
2026-06-08 11:46:23 +0500

    Update game.js

--- js/game.js
+++ js/game.js
@@ -1,7 +1,32 @@
+const params = new URLSearchParams(window.location.search);
+
+const gameId = Number(params.get("id"));
+
 const gameDetails = document.getElementById("gameDetails");
 
 gameDetails.innerHTML = `
 
-<h1>Тест работает</h1>
-<p>JavaScript успешно загружен.</p>
+<p>ID из URL: ${gameId}</p>
+<p>Количество игр: ${games.length}</p>
 `;
+
+const game = games.find(item => item.id === gameId);
+
+if (game) {
+
+```
+gameDetails.innerHTML += `
+    <h1>${game.title}</h1>
+    <p>${game.description}</p>
+`;
+```
+
+} else {
+
+```
+gameDetails.innerHTML += `
+    <h2>Игра не найдена</h2>
+`;
+```
+
+}
