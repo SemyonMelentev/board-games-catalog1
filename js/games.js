65ac6853848496d3c15a077a2c00ba661a2eadc6
SemyonMelentev <149328529+SemyonMelentev@users.noreply.github.com>
2026-06-08 09:56:31 +0500

    Create games.js

---
+++ js/games.js
@@ -0,0 +1,168 @@
+const games = [
+
+{
+    id: 1,
+    title: "Catan",
+    genre: "Экономическая",
+    players: "3-4",
+    rating: 7.1,
+    image: "https://cf.geekdo-images.com/W3Bsga_uLP9kO91gZ7H8yw/original/img.jpg",
+    description:
+        "Легендарная настольная игра, в которой игроки строят поселения, дороги и города на острове Катан, собирая ресурсы и торгуя друг с другом."
+},
+
+{
+    id: 2,
+    title: "Carcassonne",
+    genre: "Семейная",
+    players: "2-5",
+    rating: 7.4,
+    image: "https://cf.geekdo-images.com/carcassonne.jpg",
+    description:
+        "Игроки создают карту средневековой Франции, размещая тайлы с дорогами, городами и монастырями."
+},
+
+{
+    id: 3,
+    title: "Pandemic",
+    genre: "Кооперативная",
+    players: "2-4",
+    rating: 7.6,
+    image: "https://cf.geekdo-images.com/pandemic.jpg",
+    description:
+        "Командная игра, в которой участники спасают мир от смертельных эпидемий."
+},
+
+{
+    id: 4,
+    title: "Ticket to Ride",
+    genre: "Семейная",
+    players: "2-5",
+    rating: 7.4,
+    image: "https://cf.geekdo-images.com/ticket.jpg",
+    description:
+        "Постройте железнодорожную сеть через всю страну и выполните свои маршруты."
+},
+
+{
+    id: 5,
+    title: "7 Wonders",
+    genre: "Стратегия",
+    players: "3-7",
+    rating: 7.7,
+    image: "https://cf.geekdo-images.com/7wonders.jpg",
+    description:
+        "Развивайте древнюю цивилизацию, собирайте ресурсы и стройте чудеса света."
+},
+
+{
+    id: 6,
+    title: "Terraforming Mars",
+    genre: "Стратегия",
+    players: "1-5",
+    rating: 8.4,
+    image: "https://cf.geekdo-images.com/tm.jpg",
+    description:
+        "Корпорации совместно терраформируют Марс, делая его пригодным для жизни."
+},
+
+{
+    id: 7,
+    title: "Azul",
+    genre: "Семейная",
+    players: "2-4",
+    rating: 7.8,
+    image: "https://cf.geekdo-images.com/azul.jpg",
+    description:
+        "Абстрактная игра про создание красивых узоров из плитки."
+},
+
+{
+    id: 8,
+    title: "Wingspan",
+    genre: "Стратегия",
+    players: "1-5",
+    rating: 8.1,
+    image: "https://cf.geekdo-images.com/wingspan.jpg",
+    description:
+        "Соберите лучший природный заповедник для птиц."
+},
+
+{
+    id: 9,
+    title: "Gloomhaven",
+    genre: "Стратегия",
+    players: "1-4",
+    rating: 8.7,
+    image: "https://cf.geekdo-images.com/gloomhaven.jpg",
+    description:
+        "Масштабное приключение с прокачкой персонажей и тактическими боями."
+},
+
+{
+    id: 10,
+    title: "Splendor",
+    genre: "Экономическая",
+    players: "2-4",
+    rating: 7.5,
+    image: "https://cf.geekdo-images.com/splendor.jpg",
+    description:
+        "Развивайте торговую империю эпохи Возрождения."
+},
+
+{
+    id: 11,
+    title: "Root",
+    genre: "Стратегия",
+    players: "2-4",
+    rating: 8.0,
+    image: "https://cf.geekdo-images.com/root.jpg",
+    description:
+        "Ассиметричная война лесных фракций за контроль над королевством."
+},
+
+{
+    id: 12,
+    title: "Scythe",
+    genre: "Стратегия",
+    players: "1-5",
+    rating: 8.2,
+    image: "https://cf.geekdo-images.com/scythe.jpg",
+    description:
+        "Альтернативная Европа 1920-х годов с огромными боевыми мехами."
+},
+
+{
+    id: 13,
+    title: "Ark Nova",
+    genre: "Стратегия",
+    players: "1-4",
+    rating: 8.5,
+    image: "https://cf.geekdo-images.com/arknova.jpg",
+    description:
+        "Создайте самый успешный современный зоопарк."
+},
+
+{
+    id: 14,
+    title: "Codenames",
+    genre: "Карточная",
+    players: "2-8",
+    rating: 7.6,
+    image: "https://cf.geekdo-images.com/codenames.jpg",
+    description:
+        "Командная игра на ассоциации и догадки."
+},
+
+{
+    id: 15,
+    title: "Dominion",
+    genre: "Карточная",
+    players: "2-4",
+    rating: 7.7,
+    image: "https://cf.geekdo-images.com/dominion.jpg",
+    description:
+        "Классическая колодостроительная игра, ставшая основателем жанра."
+}
+
+];
