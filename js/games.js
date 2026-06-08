0da0e8db6773b168d031a0424a61e8f796e2ceae
SemyonMelentev <149328529+SemyonMelentev@users.noreply.github.com>
2026-06-08 23:35:56 +0500

    Update games.js

--- js/games.js
+++ js/games.js
@@ -1,168 +1,137 @@
 const games = [
-
 {
-    id: 1,
-    title: "Catan",
-    genre: "Экономическая",
-    players: "3-4",
-    rating: 7.1,
-    image: "https://cf.geekdo-images.com/W3Bsga_uLP9kO91gZ7H8yw/original/img.jpg",
-    description:
-        "Легендарная настольная игра, в которой игроки строят поселения, дороги и города на острове Катан, собирая ресурсы и торгуя друг с другом."
+id: 1,
+title: "Catan",
+genre: "Стратегия",
+players: "3-4",
+rating: 8.2,
+image: "images/catan.jpg",
+description: "Классическая настольная игра про торговлю, строительство и освоение острова."
 },
-
 {
-    id: 2,
-    title: "Carcassonne",
-    genre: "Семейная",
-    players: "2-5",
-    rating: 7.4,
-    image: "https://cf.geekdo-images.com/carcassonne.jpg",
-    description:
-        "Игроки создают карту средневековой Франции, размещая тайлы с дорогами, городами и монастырями."
+id: 2,
+title: "Carcassonne",
+genre: "Семейная",
+players: "2-5",
+rating: 8.0,
+image: "images/carcassonne.jpg",
+description: "Создавайте средневековые города, дороги и поля из игровых тайлов."
 },
-
 {
-    id: 3,
-    title: "Pandemic",
-    genre: "Кооперативная",
-    players: "2-4",
-    rating: 7.6,
-    image: "https://cf.geekdo-images.com/pandemic.jpg",
-    description:
-        "Командная игра, в которой участники спасают мир от смертельных эпидемий."
+id: 3,
+title: "Pandemic",
+genre: "Кооперативная",
+players: "2-4",
+rating: 8.4,
+image: "images/pandemic.jpg",
+description: "Работайте вместе, чтобы остановить распространение смертельных заболеваний."
 },
-
 {
-    id: 4,
-    title: "Ticket to Ride",
-    genre: "Семейная",
-    players: "2-5",
-    rating: 7.4,
-    image: "https://cf.geekdo-images.com/ticket.jpg",
-    description:
-        "Постройте железнодорожную сеть через всю страну и выполните свои маршруты."
+id: 4,
+title: "Ticket to Ride",
+genre: "Семейная",
+players: "2-5",
+rating: 8.1,
+image: "images/ticket-to-ride.jpg",
+description: "Стройте железнодорожные маршруты и соединяйте города по всей карте."
 },
-
 {
-    id: 5,
-    title: "7 Wonders",
-    genre: "Стратегия",
-    players: "3-7",
-    rating: 7.7,
-    image: "https://cf.geekdo-images.com/7wonders.jpg",
-    description:
-        "Развивайте древнюю цивилизацию, собирайте ресурсы и стройте чудеса света."
+id: 5,
+title: "7 Wonders",
+genre: "Карточная",
+players: "3-7",
+rating: 8.3,
+image: "images/seven-wonders.jpg",
+description: "Развивайте древнюю цивилизацию и постройте величайшее чудо света."
 },
-
 {
-    id: 6,
-    title: "Terraforming Mars",
-    genre: "Стратегия",
-    players: "1-5",
-    rating: 8.4,
-    image: "https://cf.geekdo-images.com/tm.jpg",
-    description:
-        "Корпорации совместно терраформируют Марс, делая его пригодным для жизни."
+id: 6,
+title: "Terraforming Mars",
+genre: "Стратегия",
+players: "1-5",
+rating: 8.7,
+image: "images/terraforming-mars.jpg",
+description: "Превратите Марс в пригодную для жизни планету с помощью технологий."
 },
-
 {
-    id: 7,
-    title: "Azul",
-    genre: "Семейная",
-    players: "2-4",
-    rating: 7.8,
-    image: "https://cf.geekdo-images.com/azul.jpg",
-    description:
-        "Абстрактная игра про создание красивых узоров из плитки."
+id: 7,
+title: "Azul",
+genre: "Абстрактная",
+players: "2-4",
+rating: 8.0,
+image: "images/azul.jpg",
+description: "Создавайте красивые узоры из керамической плитки."
 },
-
 {
-    id: 8,
-    title: "Wingspan",
-    genre: "Стратегия",
-    players: "1-5",
-    rating: 8.1,
-    image: "https://cf.geekdo-images.com/wingspan.jpg",
-    description:
-        "Соберите лучший природный заповедник для птиц."
+id: 8,
+title: "Wingspan",
+genre: "Семейная",
+players: "1-5",
+rating: 8.5,
+image: "images/wingspan.jpg",
+description: "Развивайте природный заповедник и собирайте редких птиц."
 },
-
 {
-    id: 9,
-    title: "Gloomhaven",
-    genre: "Стратегия",
-    players: "1-4",
-    rating: 8.7,
-    image: "https://cf.geekdo-images.com/gloomhaven.jpg",
-    description:
-        "Масштабное приключение с прокачкой персонажей и тактическими боями."
+id: 9,
+title: "Gloomhaven",
+genre: "Приключение",
+players: "1-4",
+rating: 8.8,
+image: "images/gloomhaven.jpg",
+description: "Масштабная фэнтезийная кампания с тактическими боями и развитием персонажей."
 },
-
 {
-    id: 10,
-    title: "Splendor",
-    genre: "Экономическая",
-    players: "2-4",
-    rating: 7.5,
-    image: "https://cf.geekdo-images.com/splendor.jpg",
-    description:
-        "Развивайте торговую империю эпохи Возрождения."
+id: 10,
+title: "Splendor",
+genre: "Экономическая",
+players: "2-4",
+rating: 8.0,
+image: "images/splendor.jpg",
+description: "Станьте самым успешным торговцем драгоценными камнями."
 },
-
 {
-    id: 11,
-    title: "Root",
-    genre: "Стратегия",
-    players: "2-4",
-    rating: 8.0,
-    image: "https://cf.geekdo-images.com/root.jpg",
-    description:
-        "Ассиметричная война лесных фракций за контроль над королевством."
+id: 11,
+title: "Root",
+genre: "Стратегия",
+players: "2-4",
+rating: 8.4,
+image: "images/root.jpg",
+description: "Асимметричная стратегия о борьбе лесных фракций за власть."
 },
-
 {
-    id: 12,
-    title: "Scythe",
-    genre: "Стратегия",
-    players: "1-5",
-    rating: 8.2,
-    image: "https://cf.geekdo-images.com/scythe.jpg",
-    description:
-        "Альтернативная Европа 1920-х годов с огромными боевыми мехами."
+id: 12,
+title: "Scythe",
+genre: "Стратегия",
+players: "1-5",
+rating: 8.6,
+image: "images/scythe.jpg",
+description: "Альтернативная Европа после войны, механические гиганты и развитие империи."
 },
-
 {
-    id: 13,
-    title: "Ark Nova",
-    genre: "Стратегия",
-    players: "1-4",
-    rating: 8.5,
-    image: "https://cf.geekdo-images.com/arknova.jpg",
-    description:
-        "Создайте самый успешный современный зоопарк."
+id: 13,
+title: "Ark Nova",
+genre: "Стратегия",
+players: "1-4",
+rating: 8.9,
+image: "images/ark-nova.jpg",
+description: "Создайте лучший современный зоопарк и поддерживайте проекты по защите животных."
 },
-
 {
-    id: 14,
-    title: "Codenames",
-    genre: "Карточная",
-    players: "2-8",
-    rating: 7.6,
-    image: "https://cf.geekdo-images.com/codenames.jpg",
-    description:
-        "Командная игра на ассоциации и догадки."
+id: 14,
+title: "Codenames",
+genre: "Для компании",
+players: "2-8",
+rating: 7.9,
+image: "images/codenames.jpg",
+description: "Командная игра на ассоциации, логику и интуицию."
 },
-
 {
-    id: 15,
-    title: "Dominion",
-    genre: "Карточная",
-    players: "2-4",
-    rating: 7.7,
-    image: "https://cf.geekdo-images.com/dominion.jpg",
-    description:
-        "Классическая колодостроительная игра, ставшая основателем жанра."
+id: 15,
+title: "Dominion",
+genre: "Карточная",
+players: "2-4",
+rating: 8.2,
+image: "images/dominion.jpg",
+description: "Одна из самых известных колодостроительных игр в мире."
 }
-
 ];
