7852fce1a380459f4ca89445afbf0574da849f68
SemyonMelentev <149328529+SemyonMelentev@users.noreply.github.com>
2026-06-08 10:00:01 +0500

    Create game.js

---
+++ js/game.js
@@ -0,0 +1,72 @@
+const params = new URLSearchParams(window.location.search);
+
+const gameId = Number(
+    params.get("id")
+);
+
+const game =
+    games.find(
+        item => item.id === gameId
+    );
+
+const gameDetails =
+    document.getElementById(
+        "gameDetails"
+    );
+
+if (!game) {
+
+    gameDetails.innerHTML = `
+        <h2>
+            Игра не найдена
+        </h2>
+
+        <a href="index.html"
+           class="details-btn">
+            Вернуться в каталог
+        </a>
+    `;
+
+} else {
+
+    gameDetails.innerHTML = `
+
+        <img
+            src="${game.image}"
+            alt="${game.title}"
+            onerror="this.src='https://via.placeholder.com/600x800?text=${encodeURIComponent(game.title)}'"
+        >
+
+        <h1 class="game-title">
+            ${game.title}
+        </h1>
+
+        <p class="info">
+            <strong>Жанр:</strong>
+            ${game.genre}
+        </p>
+
+        <p class="info">
+            <strong>Количество игроков:</strong>
+            ${game.players}
+        </p>
+
+        <p class="info">
+            <strong>Рейтинг:</strong>
+            ⭐ ${game.rating}
+        </p>
+
+        <div class="game-description">
+
+            <h3>
+                Описание
+            </h3>
+
+            <p>
+                ${game.description}
+            </p>
+
+        </div>
+
+    `;
+}
